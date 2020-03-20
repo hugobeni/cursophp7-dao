55524acbea60b7e26f9acaab7c7dfca9d92eb914
hugobeni <hugo.benni17@gmail.com>
2020-03-20 10:27:18 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php 
 require_once("config.php");
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root = new Usuario();
+//carega uma lista de usuarios
+//$list = Usuario::getList();
+//echo json_encode($list);
 
-$root->loadById(3);
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("t");
+//echo json_encode($search);
 
-echo $root;
+//carrega um usuario usando login e a senha
+$usuario = new Usuario();
+$usuario->login("root","auscgas");
 
-
+echo $usuario;
  ?>
